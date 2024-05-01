efe8890f4ac44e9e6f93d2476835e51e63796c25
Ayman Elomari <aelomari@Aymans-MacBook-Pro.local>
2024-05-01 17:03:38 +0100

    done

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   loading.tsx                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 16:50:00 by aelomari          #+#    #+#             */
+/*   Updated: 2024/05/01 16:57:20 by aelomari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import DashboardSkeleton from "@/app/ui/skeletons";
+export default function Loading() {
+    return <DashboardSkeleton />;
+  }
